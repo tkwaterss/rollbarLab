d0e7f7f482deb7c722f9ae7c355c0f7804f51cbf
Tobin Waters <tkwaterss@gmail.com>
2023-03-02 12:31:30 -0700

    functional site ready to deploy

---
+++ public/main.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', (event) => {
+    alert("YOU LOVE THIS PAGE");
+})
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+    res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+app.listen(4000, console.log("App running on 4000"))
