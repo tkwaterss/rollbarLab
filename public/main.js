100b67daeeced17a64f59823194a780bb6b803c9
Tobin Waters <tkwaterss@gmail.com>
2023-03-02 14:43:01 -0700

    more functionality added

--- public/main.js
+++ public/main.js
@@ -1,6 +1,27 @@
-const button = document.querySelector('button');
+const button = document.querySelector('#like');
+const getBtn = document.querySelector('#getstuff');
+const heading = document.querySelector('h1');
+const image = document.querySelector('img')
+
+let baseURL = 'http://localhost:4000'
+
 
 button.addEventListener('click', (event) => {
     alert("YOU LOVE THIS PAGE");
-    rollbar.log('button clicked')
+    axios.get(`${baseURL}/image`)
+    .then(response => {
+        let newImage = response.data;
+        image.src = newImage;
+    }).catch(err => console.log(err))
 })
+
+const getStuff = event => {
+    axios.get(`${baseURL}/stuff`)
+    .then(response => {
+        let newHeading = response.data;
+        heading.textContent = newHeading;
+    }).catch(err => console.log(err))
+}
+
+
+getBtn.addEventListener('click', getStuff)
--- server/server.js
+++ server/server.js
@@ -23,6 +23,31 @@ app.get('/', (req,res) => {
     res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+app.get('/image', (req, res) => {
+    try{
+        rollbar.log('fetching new image')
+        let images = ['./images/sincerely-media-3KEFp35FVB0-unsplash.jpg', './images/katya-ross-4Vg6ez9jaec-unsplash.jpg', './images/engin-akyurt-cIdVZMnyVv4-unsplash.jpg', './images/jan-antonin-kolar-uYmF6ncEgLY-unsplash.jpg']
+        let randomImage = images[Math.floor(Math.random() * 4)]
+        res.status(200).send(randomImage);
+    } catch (err) {
+        console.log(err);
+        rollbar.critical(err)
+    }
+})
+
+app.get('/stuff', (req,res) => {
+    try {
+        rollbar.log('new heading button clicked')
+        let headings = ['THE BEST PAGE', 'NEVER BEEN A PAGE AS GOOD AS THIS', "LAST PAGE YOU'LL EVER NEED", 'THIS IS A PAGE THAT YOU LIKE!']
+        let randomHeading = headings[Math.floor(Math.random() * 4)]
+        console.log(randomHeading)
+        res.status(200).send(randomHeading)
+    } catch (err) {
+        console.log(err)
+        rollbar.error(err)
+    }
+})
+
 rollbar.log('hello World')
 
 app.listen(4000, console.log("App running on 4000"))
